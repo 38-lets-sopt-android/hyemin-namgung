93f440b3a8c6c3f3ddffdf8c4f541e30766b93b6
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-04-08 03:38:54 +0900

    feat/#2 : DS 구현

--- app/src/main/java/com/example/letssopt/ui/theme/Color.kt
+++ app/src/main/java/com/example/letssopt/ui/theme/Color.kt
@@ -1,11 +1,15 @@
 package com.example.letssopt.ui.theme
 
+import androidx.compose.runtime.Immutable
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+@Immutable
+object LETSSOPTColors {
+    val PrimaryRed = Color(0xFFE8003C)
+    val Background = Color(0xFF141414)
+    val Surface =Color(0xFF2A2A2A)
+    val TextPrimary =Color(0xFFFFFFFF)
+    val TextSecondary = Color(0xFF999999)
+    val Placeholder =Color(0xFF666666)
+    val Disabled = Color(0xFF333333)
+}
--- app/src/main/java/com/example/letssopt/ui/theme/Type.kt
+++ app/src/main/java/com/example/letssopt/ui/theme/Type.kt
@@ -1,34 +1,67 @@
 package com.example.letssopt.ui.theme
 
+import androidx.compose.material3.Text
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.letssopt.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+data class LETSSOPTTypography(
+    val logo: TextStyle,
+    val h1: TextStyle,
+    val h2: TextStyle,
+    val body: TextStyle,
+    val caption: TextStyle
 )
+
+private val PretendartFontFamily = FontFamily(
+    Font(R.font.pretendard_regular, weight = FontWeight.Normal),
+    Font(R.font.pretendard_bold, weight = FontWeight.Bold),
+)
+
+
+private val Logo = TextStyle(
+    fontFamily = PretendartFontFamily,
+    fontWeight = FontWeight.Bold,
+    fontSize = 36.sp
+)
+
+private val H1 = TextStyle(
+    fontFamily = PretendartFontFamily,
+    fontWeight = FontWeight.Bold,
+    fontSize = 24.sp
+)
+
+private val H2 = TextStyle(
+    fontFamily = PretendartFontFamily,
+    fontWeight = FontWeight.Bold,
+    fontSize = 20.sp
+)
+
+private val Body = TextStyle(
+    fontFamily = PretendartFontFamily,
+    fontWeight = FontWeight.Normal,
+    fontSize = 16.sp
+)
+
+private val Caption = TextStyle(
+    fontFamily = PretendartFontFamily,
+    fontWeight = FontWeight.Normal,
+    fontSize = 14.sp
+)
+
+val defaultLETSSOPTTypography = LETSSOPTTypography(
+   logo =Logo,
+    h1 = H1,
+    h2 = H2,
+    body = Body,
+    caption = Caption
+)
+
+val LocalLETSSOPTTypography = staticCompositionLocalOf {
+    defaultLETSSOPTTypography
+}
