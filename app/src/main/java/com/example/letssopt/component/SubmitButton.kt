2dc9a69a43eee887f783e004d64191be6ddbdb53
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-04-09 01:18:01 +0900

    chore/#2 : 불필요한 코드 정리

--- app/src/main/java/com/example/letssopt/component/SubmitButton.kt
+++ app/src/main/java/com/example/letssopt/component/SubmitButton.kt
@@ -5,12 +5,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
@@ -54,7 +50,6 @@ fun SubmitButton(
     }
 
 }
-
 
 @Preview(showBackground = true)
 @Composable
