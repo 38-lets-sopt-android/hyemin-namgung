8cf09e0ffd63ee883db31ee5823b8110b38ac8f7
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-04-21 01:35:56 +0900

    refactor/#4 : Login 뷰모델 수정

--- app/src/main/java/com/example/letssopt/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/example/letssopt/presentation/login/LoginViewModel.kt
@@ -4,25 +4,35 @@ import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-class LoginViewModel: ViewModel(){
+class LoginViewModel : ViewModel() {
     private val _email = MutableStateFlow("")
     val email = _email.asStateFlow()
 
     private val _password = MutableStateFlow("")
     val password = _password.asStateFlow()
 
-    fun updateEmail(changeEmail : String){
+    private var registerEmail: String = ""
+    private var registerPassword: String = ""
+
+    fun updateEmail(changeEmail: String) {
         _email.value = changeEmail
     }
 
-    fun updatePassword(changePassword: String){
+    fun updatePassword(changePassword: String) {
         _password.value = changePassword
     }
 
-    fun login(inputEmail : String,
-              inputPassword : String)
-    : Boolean {
-        return _email.value == inputEmail &&
-                _password.value == inputPassword
+    fun updateRegisterUser(changeEmail: String, changePassword: String) {
+        registerEmail = changeEmail
+        registerPassword = changePassword
+    }
+
+    fun login(
+        registerEmail: String,
+        registerPassword: String
+    )
+            : Boolean {
+        return _email.value == registerEmail &&
+                _password.value == registerPassword
     }
 }
