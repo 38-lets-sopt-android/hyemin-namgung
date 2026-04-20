d1ea2661fd2276774b8ff2aaf24c7180ef461f20
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-04-21 00:51:09 +0900

    feat/#4 : Login 뷰모델 구현

---
+++ app/src/main/java/com/example/letssopt/presentation/login/LoginViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.letssopt.presentation.login
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class LoginViewModel: ViewModel(){
+    private val _email = MutableStateFlow("")
+    val email = _email.asStateFlow()
+
+    private val _password = MutableStateFlow("")
+    val password = _password.asStateFlow()
+
+    fun updateEmail(changeEmail : String){
+        _email.value = changeEmail
+    }
+
+    fun updatePassword(changePassword: String){
+        _password.value = changePassword
+    }
+
+    fun login(inputEmail : String,
+              inputPassword : String)
+    : Boolean {
+        return _email.value == inputEmail &&
+                _password.value == inputPassword
+    }
+}
